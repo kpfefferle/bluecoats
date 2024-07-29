704433a024a3e2bcb9226f5a744fb0206f16e66c
Kevin Pfefferle <kevin@pfefferle.me>
2024-07-28 20:21:09 -0400

    Add 2017 data

---
+++ app/data/2017.ts
@@ -0,0 +1,158 @@
+import { type SeasonScores } from 'bluecoats/data';
+
+export const SEASON_2017: SeasonScores = {
+  year: 2017,
+  endDate: '2017-08-12',
+  scores: [
+    {
+      date: '2017-06-22',
+      location: 'Indianapolis, IN',
+      score: 72.3,
+    },
+    {
+      date: '2017-06-24',
+      location: 'Massillon, OH',
+      score: 72.7,
+    },
+    {
+      date: '2017-06-26',
+      location: 'Hamilton, OH',
+      score: 74.6,
+    },
+    {
+      date: '2017-06-27',
+      location: 'Pittsburgh, PA',
+      score: 74.7,
+    },
+    {
+      date: '2017-07-01',
+      location: 'Lisle, IL',
+      score: 75.4,
+    },
+    {
+      date: '2017-07-02',
+      location: 'La Crosse, WI',
+      score: 76.9,
+    },
+    {
+      date: '2017-07-03',
+      location: 'Mankato, MN',
+      score: 78.3,
+    },
+    {
+      date: '2017-07-07',
+      location: 'Rockford, IL',
+      score: 79.3,
+    },
+    {
+      date: '2017-07-08',
+      location: 'Minneapolis, MN',
+      score: 80.1,
+    },
+    {
+      date: '2017-07-09',
+      location: 'Rochester, MN',
+      score: 81.8,
+    },
+    {
+      date: '2017-07-11',
+      location: 'Johnston, IA',
+      score: 82.15,
+    },
+    {
+      date: '2017-07-15',
+      location: 'Denver, CO',
+      score: 83.85,
+    },
+    {
+      date: '2017-07-17',
+      location: 'El Dorado, KS',
+      score: 85.3,
+    },
+    {
+      date: '2017-07-18',
+      location: 'Broken Arrow, OK',
+      score: 85.15,
+    },
+    {
+      date: '2017-07-20',
+      location: 'Denton, TX',
+      score: 86.35,
+    },
+    {
+      date: '2017-07-21',
+      location: 'Houston, TX',
+      score: 86.45,
+    },
+    {
+      date: '2017-07-22',
+      location: 'San Antonio, TX',
+      score: 88.025,
+    },
+    {
+      date: '2017-07-24',
+      location: 'Dallas, TX',
+      score: 89.25,
+    },
+    {
+      date: '2017-07-26',
+      location: 'Little Rock, AR',
+      score: 90.2,
+    },
+    {
+      date: '2017-07-28',
+      location: 'Nashville, TN',
+      score: 90.475,
+    },
+    {
+      date: '2017-07-29',
+      location: 'Powder Springs, GA',
+      score: 91.175,
+    },
+    {
+      date: '2017-07-30',
+      location: 'Winston-Salem, NC',
+      score: 91.2,
+    },
+    {
+      date: '2017-07-31',
+      location: 'Dublin, OH',
+      score: 92.5,
+    },
+    {
+      date: '2017-08-03',
+      location: 'Chester, PA',
+      score: 92.825,
+    },
+    {
+      date: '2017-08-04',
+      location: 'Allentown, PA',
+      score: 93.4,
+    },
+    {
+      date: '2017-08-06',
+      location: 'Buffalo, NY',
+      score: 93.725,
+    },
+    {
+      date: '2017-08-07',
+      location: 'Massillon, OH',
+      score: 94.025,
+    },
+    {
+      date: '2017-08-10',
+      location: 'Indianapolis, IN',
+      score: 94.638,
+    },
+    {
+      date: '2017-08-11',
+      location: 'Indianapolis, IN',
+      score: 95.212,
+    },
+    {
+      date: '2017-08-12',
+      location: 'Indianapolis, IN',
+      score: 95.163,
+    },
+  ],
+};
--- app/data/index.ts
+++ app/data/index.ts
@@ -1,3 +1,4 @@
+import { SEASON_2017 } from 'bluecoats/data/2017';
 import { SEASON_2018 } from 'bluecoats/data/2018';
 import { SEASON_2019 } from 'bluecoats/data/2019';
 import { SEASON_2022 } from 'bluecoats/data/2022';
@@ -16,6 +17,7 @@ export interface SeasonScores {
 }
 
 export const ALL_SEASONS: SeasonScores[] = [
+  SEASON_2017,
   SEASON_2018,
   SEASON_2019,
   SEASON_2022,
