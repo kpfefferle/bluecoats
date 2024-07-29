0e9dffe46a83b0d0a6d1fde26c8d9e3ef94329ca
Kevin Pfefferle <kevin@pfefferle.me>
2024-07-28 23:20:35 -0400

    Add 2009 data

---
+++ app/data/2009.ts
@@ -0,0 +1,148 @@
+import { type SeasonScores } from 'bluecoats/data';
+
+export const SEASON_2009: SeasonScores = {
+  year: 2009,
+  endDate: '2009-08-08',
+  scores: [
+    {
+      date: '2009-06-23',
+      location: 'Rio Rancho, NM',
+      score: 74.9,
+    },
+    {
+      date: '2009-06-24',
+      location: 'Glendale, AZ',
+      score: 74.6,
+    },
+    {
+      date: '2009-06-26',
+      location: 'Clovis, CA',
+      score: 76,
+    },
+    {
+      date: '2009-06-27',
+      location: 'Stanford, CA',
+      score: 76.2,
+    },
+    {
+      date: '2009-06-28',
+      location: 'Stockton, CA',
+      score: 77.45,
+    },
+    {
+      date: '2009-07-02',
+      location: 'Bakersfield, CA',
+      score: 79.5,
+    },
+    {
+      date: '2009-07-03',
+      location: 'Santa Barbara, CA',
+      score: 80.2,
+    },
+    {
+      date: '2009-07-05',
+      location: 'San Diego, CA',
+      score: 79.7,
+    },
+    {
+      date: '2009-07-08',
+      location: 'Ogden, UT',
+      score: 83.1,
+    },
+    {
+      date: '2009-07-10',
+      location: 'Windsor, CO',
+      score: 82.5,
+    },
+    {
+      date: '2009-07-11',
+      location: 'Denver, CO',
+      score: 84,
+    },
+    {
+      date: '2009-07-13',
+      location: 'Hutchinson, KS',
+      score: 84.95,
+    },
+    {
+      date: '2009-07-16',
+      location: 'Dallas, TX',
+      score: 85.45,
+    },
+    {
+      date: '2009-07-17',
+      location: 'Houston, TX',
+      score: 86.05,
+    },
+    {
+      date: '2009-07-18',
+      location: 'San Antonio, TX',
+      score: 86.5,
+    },
+    {
+      date: '2009-07-19',
+      location: 'Denton, TX',
+      score: 87.05,
+    },
+    {
+      date: '2009-07-21',
+      location: 'Monroe, LA',
+      score: 87.7,
+    },
+    {
+      date: '2009-07-22',
+      location: 'Hattiesburg, MS',
+      score: 88.65,
+    },
+    {
+      date: '2009-07-24',
+      location: 'Murfreesboro, TN',
+      score: 87,
+    },
+    {
+      date: '2009-07-25',
+      location: 'Atlanta, GA',
+      score: 89.8,
+    },
+    {
+      date: '2009-07-27',
+      location: 'Sevierville, TN',
+      score: 89.95,
+    },
+    {
+      date: '2009-07-29',
+      location: 'Centerville, OH',
+      score: 90.95,
+    },
+    {
+      date: '2009-07-31',
+      location: 'Allentown, PA',
+      score: 91.1,
+    },
+    {
+      date: '2009-08-02',
+      location: 'Buffalo, NY',
+      score: 92.55,
+    },
+    {
+      date: '2009-08-03',
+      location: 'Massillon, OH',
+      score: 92.4,
+    },
+    {
+      date: '2009-08-06',
+      location: 'Indianapolis, IN',
+      score: 92.65,
+    },
+    {
+      date: '2009-08-07',
+      location: 'Indianapolis, IN',
+      score: 93.55,
+    },
+    {
+      date: '2009-08-08',
+      location: 'Indianapolis, IN',
+      score: 93.15,
+    },
+  ],
+};
