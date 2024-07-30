3248d570ddb394c0844dc9a8a6ff515c539ed5c1
Kevin Pfefferle <kevin@pfefferle.me>
2024-07-29 23:29:45 -0400

    Add 1996 data

---
+++ app/data/1996.ts
@@ -0,0 +1,133 @@
+import { type SeasonScores } from 'bluecoats/data';
+
+export const SEASON_1996: SeasonScores = {
+  year: 1996,
+  endDate: '1996-08-17',
+  scores: [
+    {
+      date: '1996-06-28',
+      location: 'Butler, PA',
+      score: 66.1,
+    },
+    {
+      date: '1996-06-29',
+      location: 'Newark, DE',
+      score: 69.2,
+    },
+    {
+      date: '1996-06-30',
+      location: 'Lynn, MA',
+      score: 72,
+    },
+    {
+      date: '1996-07-03',
+      location: 'Bristol, RI',
+      score: 69.9,
+    },
+    {
+      date: '1996-07-05',
+      location: 'Kingston, NY',
+      score: 72.5,
+    },
+    {
+      date: '1996-07-08',
+      location: 'Montreal, QUE',
+      score: 74.8,
+    },
+    {
+      date: '1996-07-10',
+      location: 'Corning, NY',
+      score: 75.3,
+    },
+    {
+      date: '1996-07-13',
+      location: 'Allentown, PA',
+      score: 75,
+    },
+    {
+      date: '1996-07-16',
+      location: 'Dayton, OH',
+      score: 76.7,
+    },
+    {
+      date: '1996-07-21',
+      location: 'La Crosse, WI',
+      score: 80.6,
+    },
+    {
+      date: '1996-07-23',
+      location: 'Sioux City, IA',
+      score: 76.4,
+    },
+    {
+      date: '1996-07-25',
+      location: 'Stillwater, MN',
+      score: 79.1,
+    },
+    {
+      date: '1996-07-27',
+      location: 'Madison, WI',
+      score: 80.8,
+    },
+    {
+      date: '1996-07-28',
+      location: 'Ypsilanti, MI',
+      score: 80.9,
+    },
+    {
+      date: '1996-07-29',
+      location: 'Marion, OH',
+      score: 80.3,
+    },
+    {
+      date: '1996-07-31',
+      location: 'Canton, OH',
+      score: 83,
+    },
+    {
+      date: '1996-08-02',
+      location: 'Allentown, PA',
+      score: 81,
+    },
+    {
+      date: '1996-08-05',
+      location: 'Fort Edward, NY',
+      score: 83.7,
+    },
+    {
+      date: '1996-08-07',
+      location: 'Columbus, OH',
+      score: 85.5,
+    },
+    {
+      date: '1996-08-08',
+      location: 'Hornell, NY',
+      score: 85.7,
+    },
+    {
+      date: '1996-08-09',
+      location: 'Salem, VA',
+      score: 83.7,
+    },
+    {
+      date: '1996-08-10',
+      location: 'Atlanta, GA',
+      score: 84.5,
+    },
+    {
+      date: '1996-08-15',
+      location: 'Orlando, FL',
+      score: 87.4,
+    },
+    {
+      date: '1996-08-16',
+      location: 'Orlando, FL',
+      score: 88.1,
+    },
+    {
+      date: '1996-08-17',
+      location: 'Orlando, FL',
+      score: 86.3,
+    },
+  ],
+};
