cf201938c1a7ccd09df0c9137c3b2c9fabc40383
Kevin Pfefferle <kevin@pfefferle.me>
2024-07-30 12:41:42 -0400

    Add 1991 data

---
+++ app/data/1991.ts
@@ -0,0 +1,188 @@
+import { type SeasonScores } from 'bluecoats/data';
+
+export const SEASON_1991: SeasonScores = {
+  year: 1991,
+  endDate: '1991-08-17',
+  scores: [
+    {
+      date: '1991-06-15',
+      location: 'Canton, OH',
+      score: 64.9,
+    },
+    {
+      date: '1991-06-17',
+      location: 'Columbus, OH',
+      score: 63.7,
+    },
+    {
+      date: '1991-06-20',
+      location: 'Wyandotte, MI',
+      score: 67.3,
+    },
+    {
+      date: '1991-06-21',
+      location: 'Whitewater, WI',
+      score: 66.5,
+    },
+    {
+      date: '1991-06-22',
+      location: 'Milwaukee, WI',
+      score: 66.9,
+    },
+    {
+      date: '1991-06-23',
+      location: 'Normal, IL',
+      score: 69.7,
+    },
+    {
+      date: '1991-06-25',
+      location: 'Saginaw, MI',
+      score: 68.8,
+    },
+    {
+      date: '1991-06-26',
+      location: 'Otsego, MI',
+      score: 71.7,
+    },
+    {
+      date: '1991-06-28',
+      location: 'Mishawaka, IN',
+      score: 69.7,
+    },
+    {
+      date: '1991-06-29',
+      location: 'Charleston, IL',
+      score: 71.9,
+    },
+    {
+      date: '1991-07-05',
+      location: 'Bensalem, PA',
+      score: 66.7,
+    },
+    {
+      date: '1991-07-06',
+      location: 'Hershey, PA',
+      score: 71.6,
+    },
+    {
+      date: '1991-07-12',
+      location: 'Madison, WI',
+      score: 77.1,
+    },
+    {
+      date: '1991-07-13',
+      location: 'Rockford, IL',
+      score: 76.5,
+    },
+    {
+      date: '1991-07-14',
+      location: 'Dubuque, IA',
+      score: 79.2,
+    },
+    {
+      date: '1991-07-19',
+      location: 'DeKalb, IL',
+      score: 76.6,
+    },
+    {
+      date: '1991-07-20',
+      location: 'DeKalb, IL',
+      score: 76.6,
+    },
+    {
+      date: '1991-07-23',
+      location: 'Morgantown, WV',
+      score: 76.4,
+    },
+    {
+      date: '1991-07-24',
+      location: 'Canton, OH',
+      score: 78,
+    },
+    {
+      date: '1991-07-25',
+      location: 'Plymouth, MI',
+      score: 78.9,
+    },
+    {
+      date: '1991-07-27',
+      location: 'Madison, WI',
+      score: 80.3,
+    },
+    {
+      date: '1991-07-27',
+      location: 'Madison, WI',
+      score: 78.9,
+    },
+    {
+      date: '1991-07-29',
+      location: 'Fort Wayne, IN',
+      score: 81.8,
+    },
+    {
+      date: '1991-07-31',
+      location: 'Dublin, OH',
+      score: 83.1,
+    },
+    {
+      date: '1991-08-01',
+      location: 'Port Huron, MI',
+      score: 84,
+    },
+    {
+      date: '1991-08-03',
+      location: 'Hamilton, ONT',
+      score: 83.2,
+    },
+    {
+      date: '1991-08-03',
+      location: 'Hamilton, ONT',
+      score: 83.3,
+    },
+    {
+      date: '1991-08-05',
+      location: 'Parma, OH',
+      score: 85.6,
+    },
+    {
+      date: '1991-08-07',
+      location: 'Hornell, NY',
+      score: 84.3,
+    },
+    {
+      date: '1991-08-09',
+      location: 'Marion, OH',
+      score: 86.7,
+    },
+    {
+      date: '1991-08-10',
+      location: 'Bloomington, IN',
+      score: 86.4,
+    },
+    {
+      date: '1991-08-11',
+      location: 'Lawrence, KS',
+      score: 83.1,
+    },
+    {
+      date: '1991-08-12',
+      location: 'Tulsa, OK',
+      score: 85.9,
+    },
+    {
+      date: '1991-08-15',
+      location: 'Dallas, TX',
+      score: 86.9,
+    },
+    {
+      date: '1991-08-16',
+      location: 'Dallas, TX',
+      score: 87,
+    },
+    {
+      date: '1991-08-17',
+      location: 'Dallas, TX',
+      score: 84.4,
+    },
+  ],
+};
