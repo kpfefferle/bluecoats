626236b394bca2db5983c521b36c7317886f933e
Kevin Pfefferle <kevin@pfefferle.me>
2024-07-29 20:26:44 -0400

    Add 2006 data

---
+++ app/data/2006.ts
@@ -0,0 +1,163 @@
+import { type SeasonScores } from 'bluecoats/data';
+
+export const SEASON_2006: SeasonScores = {
+  year: 2006,
+  endDate: '2006-08-12',
+  scores: [
+    {
+      date: '2006-06-17',
+      location: 'Toledo, OH',
+      score: 70.65,
+    },
+    {
+      date: '2006-06-18',
+      location: 'Lisle, IL',
+      score: 72.7,
+    },
+    {
+      date: '2006-06-20',
+      location: 'Ankeny, IA',
+      score: 72.85,
+    },
+    {
+      date: '2006-06-21',
+      location: 'Omaha, NE',
+      score: 74,
+    },
+    {
+      date: '2006-06-22',
+      location: 'Cedar Rapids, IA',
+      score: 75.4,
+    },
+    {
+      date: '2006-06-24',
+      location: 'Columbia, MO',
+      score: 75.3,
+    },
+    {
+      date: '2006-06-25',
+      location: 'Lexington, KY',
+      score: 76.1,
+    },
+    {
+      date: '2006-06-27',
+      location: 'Evansville, IN',
+      score: 78.8,
+    },
+    {
+      date: '2006-06-30',
+      location: 'Normal, IL',
+      score: 80.15,
+    },
+    {
+      date: '2006-07-01',
+      location: 'Michigan City, IN',
+      score: 80.3,
+    },
+    {
+      date: '2006-07-02',
+      location: 'Madison, WI',
+      score: 80.15,
+    },
+    {
+      date: '2006-07-03',
+      location: 'Cedarburg, WI',
+      score: 81.9,
+    },
+    {
+      date: '2006-07-08',
+      location: 'Indianapolis, IN',
+      score: 82.35,
+    },
+    {
+      date: '2006-07-10',
+      location: 'Fairfield, OH',
+      score: 85.35,
+    },
+    {
+      date: '2006-07-12',
+      location: 'Dublin, OH',
+      score: 85.6,
+    },
+    {
+      date: '2006-07-15',
+      location: 'Battle Creek, MI',
+      score: 86.45,
+    },
+    {
+      date: '2006-07-16',
+      location: 'Centerville, OH',
+      score: 87.05,
+    },
+    {
+      date: '2006-07-18',
+      location: 'Tupelo, MS',
+      score: 87.3,
+    },
+    {
+      date: '2006-07-19',
+      location: 'West Monroe, LA',
+      score: 87.3,
+    },
+    {
+      date: '2006-07-22',
+      location: 'San Antonio, TX',
+      score: 89.05,
+    },
+    {
+      date: '2006-07-23',
+      location: 'Denton, TX',
+      score: 89.6,
+    },
+    {
+      date: '2006-07-25',
+      location: 'Wichita Falls, TX',
+      score: 90.95,
+    },
+    {
+      date: '2006-07-26',
+      location: 'Siloam Springs, AR',
+      score: 90.85,
+    },
+    {
+      date: '2006-07-29',
+      location: 'Atlanta, GA',
+      score: 90.725,
+    },
+    {
+      date: '2006-07-30',
+      location: 'Charlotte, NC',
+      score: 91.05,
+    },
+    {
+      date: '2006-07-31',
+      location: 'Massillon, OH',
+      score: 91.45,
+    },
+    {
+      date: '2006-08-02',
+      location: 'Bensalem, PA',
+      score: 92.85,
+    },
+    {
+      date: '2006-08-05',
+      location: 'Allentown, PA',
+      score: 92.35,
+    },
+    {
+      date: '2006-08-10',
+      location: 'Madison, WI',
+      score: 92.975,
+    },
+    {
+      date: '2006-08-11',
+      location: 'Madison, WI',
+      score: 93.5,
+    },
+    {
+      date: '2006-08-12',
+      location: 'Madison, WI',
+      score: 93.175,
+    },
+  ],
+};
--- app/data/index.ts
+++ app/data/index.ts
@@ -1,3 +1,4 @@
+import { SEASON_2006 } from 'bluecoats/data/2006';
 import { SEASON_2007 } from 'bluecoats/data/2007';
 import { SEASON_2008 } from 'bluecoats/data/2008';
 import { SEASON_2009 } from 'bluecoats/data/2009';
@@ -27,6 +28,7 @@ export interface SeasonScores {
 }
 
 export const ALL_SEASONS: SeasonScores[] = [
+  SEASON_2006,
   SEASON_2007,
   SEASON_2008,
   SEASON_2009,
