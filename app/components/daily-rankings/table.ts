a0d79c1ff531daa322ca53c53080ba8a305c9a53
Kevin Pfefferle <kevin@pfefferle.me>
2025-07-05 00:24:06 -0400

    Style table

--- app/components/daily-rankings/table.ts
+++ app/components/daily-rankings/table.ts
@@ -8,6 +8,20 @@ interface TableSignature {
 }
 
 export default class TableComponent extends Component<TableSignature> {
+  ordinalFor = function(rank: number): string {
+    if (Math.floor(rank / 10) === 1) {
+      return 'th';
+    } else if (rank % 10 === 1) {
+      return 'st';
+    } else if (rank % 10 === 2) {
+      return 'nd';
+    } else if (rank % 10 === 3) {
+      return 'rd';
+    } else {
+      return 'th';
+    }
+  }
+
   rankFromIndex = function(index: number): number {
     return index + 1;
   }
