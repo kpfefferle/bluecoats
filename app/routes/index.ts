50c190cc102fc09477c9989cdc775dd4e5658617
Kevin Pfefferle <kevin@pfefferle.me>
2025-07-07 13:20:53 -0400

    Fix score history query param

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import type IndexController from 'bluecoats/controllers/index';
 import { ALL_SEASONS, type SeasonScores } from 'bluecoats/data';
 
 export type IndexRouteModel = SeasonScores[];
@@ -7,11 +6,5 @@ export type IndexRouteModel = SeasonScores[];
 export default class IndexRoute extends Route {
   model(): IndexRouteModel {
     return ALL_SEASONS.filter((season) => Boolean(season.scores.length));
-  }
-
-  setupController(controller: IndexController, model: IndexRouteModel) {
-    super.setupController(controller, model);
-    let lastSeason = model.at(-1)!;
-    controller.years = lastSeason.year;
   }
 }
